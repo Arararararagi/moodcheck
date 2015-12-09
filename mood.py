1b6efbd6737ac04681f4ef368090e86367433904
I bit my tongue <Arararararagi@users.noreply.github.com>
2015-12-09 23:45:40 +0000

    Update mood.py

--- mood.py
+++ mood.py
@@ -8,17 +8,38 @@ import requests
 import nltk
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+import re
+import http.cookiejar
+import time
+
+cj = http.cookiejar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
 
+def main():
+    try:
+        page = 'http://feeds.finance.yahoo.com/rss/2.0/headline?s=GOOG&region=US&lang=en-US'
+        sourceCode = opener.open(page).read()
+        #print sourceCode
 
-'''import yahoofinance
-import fortune
-import fool
-import forbes
+        try:
+            titles = re.findall(r'',sourceCode)
+            links = re.findall(r'(.*?)',sourceCode)
+            for title in titles:
+                print (title)
+            for link in links:
+                print (link)
+        except Exception as e:
+            print (str(e))
 
-'''
-url = "http://fortune.com/2015/12/06/patriots-virtual-reality/?xid=yahoo_fortune"
-html = urlopen(url)
-beautifulhtml = BeautifulSoup(html.read(),"lxml")
-content = beautifulhtml.find("div",  {"class":"article-body-text rail-offset"})
-print(content)
+    except Exception as e:
+        print (str(e))
+        pass
+
+main()
+
+parsed_uri = urlparse(url)
+domain = "{uri.netloc}".format(uri=parsed_uri)
+print(domain)
